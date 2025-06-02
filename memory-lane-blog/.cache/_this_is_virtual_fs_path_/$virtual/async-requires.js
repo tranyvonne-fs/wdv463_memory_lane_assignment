9f9ec7afcc0abefb88ad7e4751bef5679c982ccf
devwithyvonnie <devwithyvonnie@gmail.com>
2025-06-01 17:02:17 -0700

    just trying to commit my previous changes

--- memory-lane-blog/.cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
+++
@@ -1,8 +0,0 @@
-exports.components = {
-  "component---src-pages-api-best-practices-js": () => import("./../../../src/pages/api-best-practices.js" /* webpackChunkName: "component---src-pages-api-best-practices-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-intro-to-rest-apis-js": () => import("./../../../src/pages/intro-to-rest-apis.js" /* webpackChunkName: "component---src-pages-intro-to-rest-apis-js" */),
-  "component---src-pages-learning-postman-api-testing-js": () => import("./../../../src/pages/learning-postman-api-testing.js" /* webpackChunkName: "component---src-pages-learning-postman-api-testing-js" */),
-  "component---src-pages-styled-with-tailwindcss-js": () => import("./../../../src/pages/styled-with-tailwindcss.js" /* webpackChunkName: "component---src-pages-styled-with-tailwindcss-js" */)
-}
-
